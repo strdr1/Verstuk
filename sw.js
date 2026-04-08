236ab1625401c08330bb73d1d95f7cc214d19089
strdr1 <danecc5678@gmail.com>
2026-04-09 00:10:38 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'verstuk-cache-v1';
+
+const urlsToCache = [
+    './',
+    './index.html'
+];
+
+// Установка
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Кэшируем файлы');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Активация
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
